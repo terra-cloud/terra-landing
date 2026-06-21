2b51af48ff168f33722a70d6258b9d48e2fd696f
alnaughty <z.cajurao98@gmail.com>
2026-06-21 16:27:08 +0800

    feat: update footer social links with correct URLs and add secure external tab attributes

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -20,8 +20,8 @@ class Footer extends StatelessComponent {
           div(classes: 'social-links', [
             a(
               classes: 'social-link',
-              href: '#',
-              attributes: {'aria-label': 'LinkedIn'},
+              href: 'https://www.linkedin.com/company/terra-ph-technologies-inc/',
+              attributes: {'aria-label': 'LinkedIn', 'target': '_blank', 'rel': 'noopener noreferrer'},
               [
                 Component.element(
                   tag: 'svg',
@@ -52,8 +52,8 @@ class Footer extends StatelessComponent {
             ),
             a(
               classes: 'social-link',
-              href: '#',
-              attributes: {'aria-label': 'Twitter'},
+              href: 'https://x.com/TerraServicesPH',
+              attributes: {'aria-label': 'X (Twitter)', 'target': '_blank', 'rel': 'noopener noreferrer'},
               [
                 Component.element(
                   tag: 'svg',
@@ -73,50 +73,23 @@ class Footer extends StatelessComponent {
                     Component.element(
                       tag: 'path',
                       attributes: {
-                        'd':
-                            'M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z',
+                        'd': 'M4 4l11.733 16h4.267l-11.733 -16z',
                       },
                     ),
-                  ],
-                ),
-              ],
-            ),
-            a(
-              classes: 'social-link',
-              href: '#',
-              attributes: {'aria-label': 'Github'},
-              [
-                Component.element(
-                  tag: 'svg',
-                  attributes: {
-                    'xmlns': 'http://www.w3.org/2000/svg',
-                    'width': '20',
-                    'height': '20',
-                    'viewBox': '0 0 24 24',
-                    'fill': 'none',
-                    'stroke': 'currentColor',
-                    'stroke-width': '2',
-                    'stroke-linecap': 'round',
-                    'stroke-linejoin': 'round',
-                    'class': 'lucide-custom',
-                  },
-                  children: [
                     Component.element(
                       tag: 'path',
                       attributes: {
-                        'd':
-                            'M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4',
+                        'd': 'M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772',
                       },
                     ),
-                    Component.element(tag: 'path', attributes: {'d': 'M9 18c-4.51 2-5-2-7-2'}),
                   ],
                 ),
               ],
             ),
             a(
               classes: 'social-link',
-              href: '#',
-              attributes: {'aria-label': 'Facebook'},
+              href: 'https://www.facebook.com/TerraSVCPH',
+              attributes: {'aria-label': 'Facebook', 'target': '_blank', 'rel': 'noopener noreferrer'},
               [
                 Component.element(
                   tag: 'svg',
